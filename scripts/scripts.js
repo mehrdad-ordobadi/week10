9f541e001b684c3c27f360fc01ae790ce8d2d581
mehrdad <mehrdad.ordobadi@gmail.com>
2022-11-09 14:01:03 -0800

    obj activity

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -13,9 +13,17 @@
 // } while(!isNaN(Number(name)))
 // // alert(greeting+' '+name+'!')
 // alert(`Hello ${name}`);
-let fruits = ['Apple','Banana']
-fruits.push('Orange')
-fruits.unshift('Mango')
-for (let item of fruits){
-    console.log(item)
+// let fruits = ['Apple','Banana']
+// fruits.push('Orange')
+// fruits.unshift('Mango')
+// for (let item of fruits){
+//     console.log(item)
+// }
+const person = {
+    name:['Bob', 'Smith'],
+    age:32,
+    gender:'male',
+    hobbies :['music', 'skiing']
 }
+let templ = `my name is ${person.name[0]} ${person.name[1]}. I am interested in ${person.hobbies[0]} and ${person.hobbies[1]}.`
+console.log(templ)
