31f9eb08675bafc233ec4a4a3293493fcf632dbf
mehrdad <mehrdad.ordobadi@gmail.com>
2022-11-09 13:29:29 -0800

    do while practice

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,8 +1,17 @@
 // for (let i= 0; i<11; i++) {
 //     console.log(Math.pow(2,i));
 // }
-let i = 0;
-while(i<11){
-    console.log(Math.pow(2,i));
-    i++
-}
+// let i = 0;
+// while(i<11){
+//     console.log(Math.pow(2,i));
+//     i++
+// }
+let greeting = "Welcome";
+let name;
+
+do{
+    name = prompt("Enter your name pls:");
+
+} while(!isNaN(Number(name)))
+// alert(greeting+' '+name+'!')
+alert(`Hello ${name}`);
