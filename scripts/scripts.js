3812d00666a555a656c24de0098c717b05d65d91
mehrdad <mehrdad.ordobadi@gmail.com>
2022-11-09 12:41:50 -0800

    use for and while loop for powers of 2

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,2 +1,8 @@
-for (let i = 0; i<1000;i++)
-console.log(i)
+// for (let i= 0; i<11; i++) {
+//     console.log(Math.pow(2,i));
+// }
+let i = 0;
+while(i<11){
+    console.log(Math.pow(2,i));
+    i++
+}
