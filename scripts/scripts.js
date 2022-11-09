144555b065e03735d72c56fcc2addfbe612465f4
mehrdad <mehrdad.ordobadi@gmail.com>
2022-11-09 14:10:49 -0800

    obj looping

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -19,11 +19,36 @@
 // for (let item of fruits){
 //     console.log(item)
 // }
-const person = {
-    name:['Bob', 'Smith'],
-    age:32,
-    gender:'male',
-    hobbies :['music', 'skiing']
+// const person = {
+//     name:['Bob', 'Smith'],
+//     age:32,
+//     gender:'male',
+//     hobbies :['music', 'skiing']
+// }
+// let templ = `my name is ${person.name[0]} ${person.name[1]}. I am interested in ${person.hobbies[0]} and ${person.hobbies[1]}.`
+// console.log(templ)
+let student1 = {
+    ID: "A00022",
+    GPA: 3.0,
+    program: "CIT"
+};
+
+let student2 = {
+    ID: "A01000",
+    GPA: 3.1,
+    program: "CST"
+};
+
+let student3 = {
+    ID: "A00114",
+    GPA: 3.2,
+    program: "CIT"
+};
+let students = [student1, student2, student3]; 
+let counter = 0;
+for (let item of students){
+    if (item.program === 'CIT' && item.GPA > 3){
+        counter++;
+    }
 }
-let templ = `my name is ${person.name[0]} ${person.name[1]}. I am interested in ${person.hobbies[0]} and ${person.hobbies[1]}.`
-console.log(templ)
+console.log(counter)
